f3908856b514fb6ba14471d8be1bf91a761f8618
Ismaila Bulcourt <ismaila.bulcourt@viacesi.fr>
2022-09-21 15:17:56 +0200

    test-service set-up

---
+++ test-service/test-Schema.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const bddConnect = require('./bdd')
+bddConnect('electiveProject')
+
+const freezbeeSchema = new mongoose.Schema({
+    nom : [String],
+    description : [String],
+    pUHT : [String],
+    gamme : Number,
+    ingredient : Array,
+    grammage : Array,
+})
+
+const ingredientSchema = new mongoose.Schema({
+    nom : [String],
+    description : [String]
+})
+
+const procedeSchema = new mongoose.Schema({
+    nom : [String],
+    description : [String],
+    modele : [String],
+    etapes : [String],
+    validation : Boolean,
+    tests : [String]
+})
+
+const procedeModel = mongoose.model('procede', procedeSchema)
+const ingredientModel = mongoose.model('ingredient', ingredientSchema)
+const freezbeeModel = mongoose.model('freezbee', freezbeeSchema)
+
+module.exports={freezbeeModel,procedeModel,ingredientModel}
